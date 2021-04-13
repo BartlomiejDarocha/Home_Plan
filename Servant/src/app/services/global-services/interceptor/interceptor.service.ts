b3e78b0985f605ac0ffd15fc446ece4b1dffb76a
LAPTOP\Bart <bartlomiejdarocha@gmail.com>
2021-04-13 22:11:17 +0200

    done basic interceptor

--- Servant/src/app/services/global-services/interceptor/interceptor.service.ts
+++ Servant/src/app/services/global-services/interceptor/interceptor.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { LoaderService } from '../loader-service/loader.service';
-import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,17 @@ export class InterceptorService implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.loaderService.showLoader();
     const token: string = localStorage.getItem('token');
-
-
+    // dodać token gdy już zrobię logowanie oraz Serwice do localStore
+    request = request.clone({ headers: this.headers });
+    if (token) {
+      request = request.clone({headers: request.headers.set('Authorization', 'test' + token )});
+    }
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // dodać obłsugę błędów jak już zorbię utilsy;
+        return throwError(error);
+      })
+    )
   }
 }
 
