87fc5b756b6479881f7dd47b9080f0670f712b3a
LAPTOP\Bart <bartlomiejdarocha@gmail.com>
2021-04-13 21:19:10 +0200

    build interceptor

--- Servant/src/app/services/global-services/interceptor/interceptor.service.ts
+++ Servant/src/app/services/global-services/interceptor/interceptor.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { LoaderService } from '../loader-service/loader.service';
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InterceptorService {
+export class InterceptorService implements HttpInterceptor {
+  private headers = new HttpHeaders({
+    'Content-type': 'application/json'
+  })
+  constructor(private loaderService: LoaderService) { }
 
-  constructor() { }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.loaderService.showLoader();
+    const token: string = localStorage.getItem('token');
+
+
+  }
 }
+
