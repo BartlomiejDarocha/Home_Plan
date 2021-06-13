98e91f3f77924b87a45a1ef0431cec41ae7b27fa
LAPTOP\Bart <bartlomiejdarocha@gmail.com>
2021-06-13 22:34:24 +0200

    Usunięcie niepotrzebnych importów

--- Servant/src/app/app.component.ts
+++ Servant/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { ApiService } from './services/global-services/api-service/api.service';
-import { LoaderService } from './services/global-services/loader-service/loader.service';
-import { UtilsService } from './services/global-services/utils-service/utils.service';
 
 @Component({
   selector: 'app-root',
