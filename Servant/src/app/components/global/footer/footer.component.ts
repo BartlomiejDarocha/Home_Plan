17e1680db3d478c60b29bdf18ec971e1a6994d52
BartekDarocha <bartlomiejdarocha@gmail.com>
2021-07-18 10:52:02 +0200

    Dodawanie ikonek

--- Servant/src/app/components/global/footer/footer.component.ts
+++ Servant/src/app/components/global/footer/footer.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public poweredList = [
+    {name: 'Angular', icon:'', url: ''},
+    {name: 'Angular materials', icon:'', url: ''},
+    {name: 'RxJs', icon:'', url: ''},
+    {name: 'Node', icon:'', url: ''},
+  ];
 
   constructor() { }
 
