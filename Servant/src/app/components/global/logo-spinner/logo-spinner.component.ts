4cbb3bc5ee79bdbc54cdddb5ebee287fa769c84d
BartekDarocha <bartlomiejdarocha@gmail.com>
2021-07-29 19:09:53 +0200

    podstawowy spinner

--- Servant/src/app/components/global/logo-spinner/logo-spinner.component.ts
+++ Servant/src/app/components/global/logo-spinner/logo-spinner.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { LoaderService } from 'src/app/services/global-services/loader-service/loader.service';
 
 @Component({
   selector: 'app-logo-spinner',
   templateUrl: './logo-spinner.component.html',
   styleUrls: ['./logo-spinner.component.scss']
 })
-export class LogoSpinnerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class LogoSpinnerComponent {
+  public isLoading: Subject<boolean> = this.loaderService.getLoader;
+  constructor(private loaderService: LoaderService) { }
 }
