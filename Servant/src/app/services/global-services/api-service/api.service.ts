7481aa5e16b92d4202fef2e51e9d8f5bb99679b3
BartekDarocha <bartlomiejdarocha@gmail.com>
2022-01-10 20:53:29 +0100

    Dodanie Get z params

--- Servant/src/app/services/global-services/api-service/api.service.ts
+++ Servant/src/app/services/global-services/api-service/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { URLSearchParams } from 'url';
@@ -30,22 +30,29 @@ export class ApiService {
     return body.toString();
   }
 
-  public get(url: string): Observable<any> {
+  public get(url: string, params: any = null): Observable<any> {
+    if (params) {
+      return this.httpClient.get(url, { params } )
+    }
     return this.httpClient.get(url);
   }
 
-  public delete(url: string): Observable<any> {
-    return this,this.httpClient.delete(url);
+  public getNotLoader(url: string, params: any = null): Observable<any> {
+    return 
   }
 
   public post(url, body: any): Observable<any> {
     // Ustalić przy budowaniu servera jakie dane będę wysyłać i odpowiednio je obrobić przed wysyłką na server 
-    return this.httpClient.post(url, body);
+    return this.httpClient.post(url, this.dataToSend(body));
   }
 
   public put(url, body: any): Observable<any> {
     // Ustalić przy budowaniu servera jakie dane będę wysyłać i odpowiednio je obrobić przed wysyłką na server 
-    return this.httpClient.put(url, body);
+    return this.httpClient.put(url, this.dataToSend(body));
+  }
+
+  public delete(url: string): Observable<any> {
+    return this,this.httpClient.delete(url);
   }
 
 }
