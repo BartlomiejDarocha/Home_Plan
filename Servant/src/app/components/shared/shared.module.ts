d4ea4c878899160c09a7636bad7182de2d646456
BartekDarocha <bartlomiejdarocha@gmail.com>
2022-01-16 19:21:23 +0100

    Popranwie modułowości, przeniesienie header oraz footer do osobnych modułów

--- Servant/src/app/components/shared/shared.module.ts
+++ Servant/src/app/components/shared/shared.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from '../header/header.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from '../footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     MatDialogModule,
     MatButtonModule
   ],
-  exports: [
-    HeaderComponent,
-    FooterComponent
-  ]
+  exports: []
 })
 export class SharedModule { }
