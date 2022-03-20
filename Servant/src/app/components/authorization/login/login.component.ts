c583258fdf10413fb51408762c776af6017d1a0e
BartekDarocha <bartlomiejdarocha@gmail.com>
2022-03-21 00:07:34 +0100

    obsługa oczka

--- Servant/src/app/components/authorization/login/login.component.ts
+++ Servant/src/app/components/authorization/login/login.component.ts
@@ -7,6 +7,7 @@ import { LoginDataModel } from 'src/app/models/login/login.model';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public showPassword = false;
   public loginData: LoginDataModel = {
     email: '',
     password: ''
@@ -18,7 +19,14 @@ export class LoginComponent implements OnInit {
   }
 
   public changeEye(target: any): void {
-    console.log(target, 'target');
+    console.log(target.target);
+    this.showPassword = !target.target.className.includes('eye_icon');
+    console.log(this.showPassword, 'showPassword');
+    if(this.showPassword) {
+      //zrób to w html i css
+      target.target.classList.add('dupa');
+      target.target.classList.remove('eye_icon');
+    }
   }
 
   public login(): void {
