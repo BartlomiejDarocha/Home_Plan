2acc7afdbd4eeb9fe410bc86357af8f1295539bf
BartekDarocha <bartlomiejdarocha@gmail.com>
2022-06-04 15:36:58 +0200

    budowanie derektywy

--- Servant/src/app/directives/forms-validation-directive/forms-validation.directive.ts
+++ Servant/src/app/directives/forms-validation-directive/forms-validation.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
@@ -9,11 +9,16 @@ export class FormsValidationDirective implements OnInit, OnDestroy {
   private subs: Subscription[] = [];
   private inputs: HTMLInputElement[] = null;
 
-  constructor(private element: ElementRef, private form: NgForm ) { }
+  constructor(
+    private element: ElementRef,
+    private form: NgForm,
+    private render: Renderer2
+  ) { }
+  // parentElement
 
   ngOnInit(): void {
     const formSub = this.form.ngSubmit.subscribe((ev: any ) => {
-      this.renderErrors();
+      this.setErrors();
     });
     this.subs.push(formSub);
   }
@@ -22,15 +27,26 @@ export class FormsValidationDirective implements OnInit, OnDestroy {
     this.subs.filter(sub => sub).forEach(sub => sub.unsubscribe());
   }
 
-  private renderErrors(): void {
+  private setErrors(): void {
     const incorrectInputs = this.checkIncorrectInputs();
     const htmlElements = this.element.nativeElement.elements;
     if (!incorrectInputs.length || !htmlElements.length) {
-      console.log('return');
       return;
     }
-    console.log(incorrectInputs, 'incorrectInputs');
     console.log(htmlElements, 'htmlElements');
+    incorrectInputs.forEach((incorrectInput: { name: string, errors: any }) => {      
+      if(!htmlElements[incorrectInput.name]) {
+        return;
+      }
+      console.log(htmlElements[incorrectInput.name], 'pokazanie tego inputa');
+      this.renderError();
+    });
+  }
+
+  private renderError(): void {
+    const error = this.render.createElement('div');
+    console.log(error, 'error');
+    // <div *ngIf="email.hasError('required') && email.touched" class="form_error">E-mail jest wymagany</div> -->
   }
 
   private checkIncorrectInputs(): { name: string, errors: any }[] {    
