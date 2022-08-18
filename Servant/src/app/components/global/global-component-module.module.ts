a1ca5e74382f3c0d371dc70926f7b04e3999ca29
BartekDarocha <bartlomiejdarocha@gmail.com>
2022-08-18 22:49:04 +0200

    Deleted unnesesery modules

--- Servant/src/app/components/global/global-component-module.module.ts
+++ Servant/src/app/components/global/global-component-module.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoaderGlobalComponent } from './loader-global/loader-global.component';
 import { SketchComponent } from './sketch/sketch.component';
 
 @NgModule({
   declarations: [
+    HeaderComponent,
+    FooterComponent,
     LoaderGlobalComponent,
     HomeComponent,
     SketchComponent,
@@ -15,6 +18,8 @@ import { SketchComponent } from './sketch/sketch.component';
     SharedModule,
   ],
   exports: [
+    HeaderComponent,
+    FooterComponent,
     LoaderGlobalComponent,
     SketchComponent,
   ]
