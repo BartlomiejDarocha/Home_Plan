6f7eac64f2d7d8b42642b8a3a593da44c111b02c
BartekDarocha <bartlomiejdarocha@gmail.com>
2022-08-25 22:51:32 +0200

    Build registery component 2

--- Servant/src/app/components/authorization/registery/registery.component.ts
+++ Servant/src/app/components/authorization/registery/registery.component.ts
@@ -7,9 +7,17 @@ import { EmailPattern } from 'src/app/models/patterns/patterns';
   styleUrls: ['./registery.component.scss']
 })
 export class RegisteryComponent implements OnInit {
+  public emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  public registeryData = {
+    email: '',
+    password: '',
+    passwordRepeat: '',
+  }
 
   ngOnInit(): void {
   }
+
+  
 
   public registery() {
     console.log('refistery');
