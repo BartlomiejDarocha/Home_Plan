ee8be41fe7efaaacf5fcc96be48e3f1f7f12c99a
BartekDarocha <bartlomiejdarocha@gmail.com>
2022-08-31 01:08:08 +0200

    Preperd function, next stage implements and tests

--- Servant/src/app/directives/form-custom-validators-directives/check-password-repeat.directive.ts
+++ Servant/src/app/directives/form-custom-validators-directives/check-password-repeat.directive.ts
@@ -1,5 +1,5 @@
-import { Directive } from '@angular/core';
-import { ValidatorFn, AbstractControl } from '@angular/forms';
+import { Directive, Input } from '@angular/core';
+import { ValidatorFn, AbstractControl, NG_VALIDATORS, Validator } from '@angular/forms';
 
 export function checkSamePasswordCorrectValidator(passwordValue: string): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} => {
@@ -9,10 +9,14 @@ export function checkSamePasswordCorrectValidator(passwordValue: string): Valida
 }
 
 @Directive({
-  selector: '[appCheckPasswordRepeat]'
+  selector: '[appCheckPasswordRepeat]',
+  providers: [{provide: NG_VALIDATORS, useExisting: 
+  CheckPasswordRepeatDirective, multi: true}]
 })
-export class CheckPasswordRepeatDirective {
+export class CheckPasswordRepeatDirective implements Validator {
+  @Input('passwordValue') passwordValue: string;
 
-  constructor() { }
-
+  validate(control: AbstractControl): {[key: string]: any} {
+    return this.passwordValue ? checkSamePasswordCorrectValidator(this.passwordValue)(control): null;
+  }
 }
